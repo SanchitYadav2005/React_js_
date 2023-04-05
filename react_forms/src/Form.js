2ab8fde954fb50ab9c29852fa94b61a26c0c0dc4
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-05 20:53:27 +0530

    form is working fine

--- react_forms/src/Form.js
+++ react_forms/src/Form.js
@@ -4,21 +4,28 @@ class Form extends Component{
     constructor(props){
         super(props);
         this.state = {username: ""}
-        this.handleChange = this.handleChange.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleChange(evt){
         this.setState({username: evt.target.value})
     }
+    handleSubmit(evt){
+        evt.preventDefault();
+        alert(`User name:- ${this.state.username}`);
+        this.setState({username: ""})
+    }
     render(){
         return(
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input 
                         type='text'
                         value={this.state.username}
                         onChange={this.handleChange}
                     />
+                    <button>Submit!</button>
                 </form>
             </div>
         )
