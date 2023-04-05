1cf496638b52fe448ceb3bfe8236a20ff7d62c10
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-05 20:48:55 +0530

    created a basic form

---
+++ react_forms/src/Form.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class Form extends Component{
+    constructor(props){
+        super(props);
+        this.state = {username: ""}
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(evt){
+        this.setState({username: evt.target.value})
+    }
+    render(){
+        return(
+            <div>
+                <form>
+                    <input 
+                        type='text'
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default Form;
