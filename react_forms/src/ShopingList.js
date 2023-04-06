4a7aceec7e92616ca8da23751c25820d8f62ed13
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-06 20:26:34 +0530

    created a static list to showcase

---
+++ react_forms/src/ShopingList.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+class ShopingList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            iteams:[
+                {name: "tomato", qty: "3"},
+                {name: "apple", qty: "5"}
+            ],
+            // this.renderItems = this.renderItems.bind(this)
+        }
+    }
+    renderIteams() {
+        return (
+          <ul>
+            {this.state.iteams.map(iteam => (
+              <li key={iteam.id}>
+                {iteam.name}: {iteam.qty}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+      render() {
+        return (
+          <div>
+            <h1>Shopping List</h1>
+            {this.renderIteams()}
+            {/* <ShoppingListForm addItem={this.addItem} /> */}
+          </div>
+        );
+      }
+}
+
+export default ShopingList;
