cb9f1cf7981959565ef0ec9ae428a43bc9f5e0d3
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-08 20:15:51 +0530

    added a return div in the Todo component

---
+++ todo_exercise/src/Todo.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+class Todo extends Component{
+    render(){
+        return(
+            <div>
+                <h1>{this.props.work}</h1>
+            </div>
+        )
+    }
+}
