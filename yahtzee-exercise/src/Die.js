50bfc77dd0847793fd4acc60d27809b78cbb378c
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-17 20:22:59 +0530

    added disable handling into it's own funciton

--- yahtzee-exercise/src/Die.js
+++ yahtzee-exercise/src/Die.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this)
+  }
+  // this is different from other because it's giving back the index that we are passing as a prop in our dice component.
+  handleClick(){
+    this.props.handleClick(this.props.idx)
+  }
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={()=>this.props.handleClick(this.props.idx)}
+        onClick={this.handleClick}
       >
         {this.props.val}
       </button>
