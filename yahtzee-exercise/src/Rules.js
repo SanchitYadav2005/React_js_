42dd5663a2be6c065b5de12ea21a4ab2867f19f1
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-17 20:48:24 +0530

    full house rule is done

--- yahtzee-exercise/src/Rules.js
+++ yahtzee-exercise/src/Rules.js
@@ -57,8 +57,12 @@ class SumDistro extends Rule {
 
 /** Check if full house (3-of-kind and 2-of-kind) */
 
-class FullHouse {
+class FullHouse extends Rule {
   // TODO
+  evalRoll = (dice)=>{
+    const freqs = this.freq(dice)
+    return freqs.includes(2) && freqs.includes(3) ? this.score : 0
+  }
 }
 
 /** Check for small straights. */
@@ -100,7 +104,7 @@ const threeOfKind = new SumDistro({ count: 3 });
 const fourOfKind = new SumDistro({ count: 4 });
 
 // full house scores as flat 25
-const fullHouse = "TODO";
+const fullHouse = new FullHouse({score: 25});
 
 // small/large straights score as 30/40
 const smallStraight = "TODO";
