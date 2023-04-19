99d3ec493773daf387745b36a01e8576d5ee979a
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-19 21:24:42 +0530

    added a loder

--- react_lifecycle_methods/src/ZenQuote.js
+++ react_lifecycle_methods/src/ZenQuote.js
@@ -1,24 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import './ZenQuotes.css'
 
-class ZenQuote extends Component{
-    constructor(props){
+class ZenQuote extends Component {
+    constructor(props) {
         super(props);
-        this.state = {quote: ""}
+        this.state = { quote: "" }
     }
-    componentDidMount(){
-        axios.get("https://api.github.com/zen").then(response =>{
-          this.setState({quote: response.data})  
+    componentDidMount() {
+        axios.get("https://api.github.com/zen").then(response => {
+            this.setState({ quote: response.data })
         })
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
+                <div class="lds-default">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
                 Alwasys remember ....
-                <h1>{this.state.quote}</h1>
+                 <h1>{this.state.quote}</h1>
             </div>
         )
     }
 }
 
-export default ZenQuote;
+                export default ZenQuote;
