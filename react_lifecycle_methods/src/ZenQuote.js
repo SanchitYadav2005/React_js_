81ffbac9c6d12dce9f2128432ea418c2afebcdee
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-19 21:10:09 +0530

    done

--- react_lifecycle_methods/src/ZenQuote.js
+++ react_lifecycle_methods/src/ZenQuote.js
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class ZenQuote extends Component{
+    constructor(props){
+        super(props);
+        this.state = {quote: ""}
+    }
     componentDidMount(){
         axios.get("https://api.github.com/zen").then(response =>{
-            console.log(response);
+          this.setState({quote: response.data})  
         })
     }
     render(){
         return(
             <div>
                 Alwasys remember ....
+                <h1>{this.state.quote}</h1>
             </div>
         )
     }
