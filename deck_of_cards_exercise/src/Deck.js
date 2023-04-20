70f049aa7aebdd2a2f81437c27851ee8ab4e12e3
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-20 20:36:03 +0530

    installed axios

---
+++ deck_of_cards_exercise/src/Deck.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Deck extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Card dealer</h1>
+            </div>
+        )
+    }
+}
+
+export default Deck;
