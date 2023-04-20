73f2afbd1b98abce0d656b2a7d9765b5ba974297
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-20 20:41:04 +0530

    api is sending the response

--- deck_of_cards_exercise/src/Deck.js
+++ deck_of_cards_exercise/src/Deck.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+const API_URL = "https://www.deckofcardsapi.com/api/deck/new/shuffle";
 
 class Deck extends Component{
+    constructor(props){
+        super(props);
+        this.state = {deck: null}
+    }
+    componentDidMount(){
+        axios.get(API_URL).then(response=>{
+            console.log(response.data)
+        })
+    }
     render(){
         return(
             <div>
