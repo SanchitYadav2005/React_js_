28d7c2140a90a123b3c219223e85534eb0129d59
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-20 21:18:43 +0530

    done with the deck and getting the data there is one problem in the componentDidMount as we are asyncing it so there is a delay in the setState so when we click on the button the deck is set to null as well as deck_id is also empty so its loads after some delay

--- deck_of_cards_exercise/src/Deck.js
+++ deck_of_cards_exercise/src/Deck.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-const API_URL = "https://www.deckofcardsapi.com/api/deck/new/shuffle";
+const BASE_API_URL = "https://www.deckofcardsapi.com/api/deck/";
 
 class Deck extends Component{
     constructor(props){
         super(props);
-        this.state = {deck: null}
+        this.state = {deck: null, drawn: []}
+        this.getCard = this.getCard.bind(this)
     }
-    componentDidMount(){
-        axios.get(API_URL).then(response=>{
-            console.log(response.data)
-        })
+    async componentDidMount(){
+        const deck = await axios.get(`${BASE_API_URL}/new/shuffle`);
+        this.setState({deck: deck.data})    
+    }
+    async getCard(){
+        let id = this.state.deck.deck_id;
+        const cardUrl = `${BASE_API_URL}/${id}/draw`;
+        let cardRes = await axios.get(cardUrl)
+        console.log(cardRes)
+        let card = cardRes.data.cards[0];
+        this.setState(st => ({
+            drawn: [
+                ...st.drawn,
+                {
+                    id: card.code,
+                    image: card.image,
+                    name: `${card.suit} of ${card.value}`
+                }
+            ]
+        }))
     }
     render(){
         return(
             <div>
                 <h1>Card dealer</h1>
+                <button onClick={this.getCard}>Get Card!</button>
             </div>
         )
     }
