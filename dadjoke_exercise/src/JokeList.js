d304865b95284178919d39211e9a5889f4b2c4ca
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-24 21:19:58 +0530

    added image and button now working on the styling on the joke list component

--- dadjoke_exercise/src/JokeList.js
+++ dadjoke_exercise/src/JokeList.js
@@ -1,38 +1,49 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './JokeList.css';
 
 
-class JokeList extends Component{
+class JokeList extends Component {
     static defaultProps = {
-        jokesToGet: 10  
+        jokesToGet: 10
     }
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             jokes: []
         }
     }
-    async componentDidMount(){
+    async componentDidMount() {
         let jokes = [];
-        while(jokes.length < this.props.jokesToGet){
-            let res = await axios.get("https://icanhazdadjoke.com/",{
-            headers: {Accept: 'application/json'}
-        });
+        while (jokes.length < this.props.jokesToGet) {
+            let res = await axios.get("https://icanhazdadjoke.com/", {
+                headers: { Accept: 'application/json' }
+            });
             jokes.push(res.data.joke)
         }
-        this.setState({jokes: jokes})
+        this.setState({ jokes: jokes })
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className='JokeList'>
-                <h1>Dad joke</h1>
+                <div className='JokeList-sidebar'>
+                    <h1 className='JokeList-title'>
+                        <span>Dad</span> Jokes
+                    </h1>
+                    <img
+                        src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+                        alt="Crying Laughing Emoji"
+                    />
+                    <button className='JokeList-getMore'>New Jokes</button>
+                </div>
+
                 <div className='JokeList-jokes'>
                     {this.state.jokes.map(joke => (
                         <div>{joke}</div>
                     ))}
                 </div>
             </div>
-            
+
         )
     }
 }
