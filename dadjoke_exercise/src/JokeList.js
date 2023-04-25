73a80e4cc9a714600a076ca2d2b864c972f2b881
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-25 21:36:49 +0530

    added a loading spinner

--- dadjoke_exercise/src/JokeList.js
+++ dadjoke_exercise/src/JokeList.js
@@ -12,8 +12,11 @@ class JokeList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
+            // here we are parsing the data that we have in our local storage.
+            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+            loading: false
         }
+        this.handleClick = this.handleClick.bind(this);
     }
     componentDidMount() {
         if(this.state.jokes.length === 0) this.getJokes()    
@@ -30,21 +33,38 @@ class JokeList extends Component {
                 votes: 0
             })
         }
-        this.setState({ jokes: jokes })
+        this.setState(st=>({
+            loading: false,
+            jokes: [...st.jokes, ...jokes]
+        }),
         //The JSON.stringify() static method converts a JavaScript value to a JSON string
         // window.localStorage is a local storage.
         // we are using it to store the jokes in local storage.
         //The localStorage object stores data with no expiration date. The data is not deleted when the browser is closed, and are available for future sessions.
-        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        ()=> window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        )
     }
     handleVote(id, delta){
         this.setState(st=>({
             jokes: st.jokes.map(j=>(
                 j.id === id ? {...j, votes: j.votes+delta}:j
             ))
-        }))
+        }),
+        ()=> window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        )
+    }
+    handleClick(){
+        this.setState({loading: true}, this.getJokes)
     }
     render() {
+        if (this.state.loading) {
+            return (
+              <div className="JokeList-spinner">
+                <i className="far fa-8x fa-laugh fa-spin" />
+                <h1 className="JokeList-title">Loading...</h1>
+              </div>
+            );
+          }
         return (
             <div className='JokeList'>
                 <div className='JokeList-sidebar'>
@@ -55,7 +75,7 @@ class JokeList extends Component {
                         src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
                         alt="Crying Laughing Emoji"
                     />
-                    <button className='JokeList-getMore'>New Jokes</button>
+                    <button className='JokeList-getMore' onClick={this.handleClick}>New Jokes</button>
                 </div>
 
                 <div className='JokeList-jokes'>
