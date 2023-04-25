37f0f708401abf3aaa54f35d10110529ba3d9980
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-25 20:55:43 +0530

    getEmoji() function is working

--- dadjoke_exercise/src/Joke.js
+++ dadjoke_exercise/src/Joke.js
@@ -2,23 +2,6 @@ import React, { Component } from 'react';
 import './Joke.css'
 
 class Joke extends Component{
-    getColor() {
-        if (this.props.votes >= 15) {
-          return "#4CAF50";
-        } else if (this.props.votes >= 12) {
-          return "#8BC34A";
-        } else if (this.props.votes >= 9) {
-          return "#CDDC39";
-        } else if (this.props.votes >= 6) {
-          return "#FFEB3B";
-        } else if (this.props.votes >= 3) {
-          return "#FFC107";
-        } else if (this.props.votes >= 0) {
-          return "#FF9800";
-        } else {
-          return "#f44336";
-        }
-      }
       getEmoji() {
         if (this.props.votes >= 15) {
           return "em em-rolling_on_the_floor_laughing";
@@ -48,7 +31,7 @@ class Joke extends Component{
                     {this.props.text}
                 </div>
                 <div className='Joke-smiley'>
-                    <i className='em em-rolling_on_the_floor_laughing'/>
+                    <i className={this.getEmoji()}/>
                 </div>
             </div>
         )
