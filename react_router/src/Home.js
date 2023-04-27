29ce826a16a772bd63e55a9c45ad983e29e81787
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-27 20:25:46 +0530

    added our App component inside BrowserRouter form react-router-dom

---
+++ react_router/src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component{
+    render(){
+        <div>
+            <h1>Here you can checkout cars.</h1>
+        </div>
+    }
+}
+
+export default Home;
