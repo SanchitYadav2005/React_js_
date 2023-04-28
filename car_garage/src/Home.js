88de1d107469ce467024087d1794a8650e0420f4
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-28 21:01:30 +0530

    created home component and workin on navbar component

---
+++ car_garage/src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component{
+    render() {
+        return (
+             <div>
+                <h1 className='text-4xl py-64'>Welcome to car garage ! Here you can find best cars</h1>
+             </div>
+        );
+    }
+}
+
+export default Home;
