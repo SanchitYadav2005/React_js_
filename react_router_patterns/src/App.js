ffe25acae3dd861797aa4cc496034fb79460d5bf
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-30 21:29:11 +0530

    paramater is not setting up

--- react_router_patterns/src/App.js
+++ react_router_patterns/src/App.js
@@ -1,10 +1,18 @@
 import Fruit from './Fruits';
 import './App.css';
+import {Routes, Route, useParams} from 'react-router-dom';
 
 function App() {
+  let {name} = useParams();
   return (
     <div className="App">
-      <Fruit name="apple"/>
+      <Routes>
+        <Route 
+          exact 
+          path='/fruit/:name'
+          element={<Fruit fruit={name}/>}
+        />
+      </Routes>
     </div>
   );
 }
--- react_router_patterns/src/Fruits.js
+++ react_router_patterns/src/Fruits.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 
 class Fruit extends Component{
     render() {
-        const url = `https://source.unsplash.com/1600x900/?${this.props.name}`
+        const url = `https://source.unsplash.com/1600x900/?${this.props.fruit}`
         return (
              <div className='Fruid'>
-                <h1>I love to eat this fruit {this.props.name}</h1>
-                <img src={url} alt={this.props.name}/>
+                <h1>I love to eat this fruit {this.props.fruit}</h1>
+                <img src={url} alt={this.props.fruit}/>
              </div>
         );
     }
