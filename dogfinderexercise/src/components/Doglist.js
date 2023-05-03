ced7db5c9af467c05ebfe714795dd6e306b73ff0
unknown <sanchit2005yudavanshi@gmail.com>
2023-05-03 20:43:32 +0530

    displying the images but these are too large

--- dogfinderexercise/src/components/Doglist.js
+++ dogfinderexercise/src/components/Doglist.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class Doglist extends Component{
     render() {
         return (
-             <div>
+             <div className='Doglist'>
                 <h1 className='display-1 text-center'>Dog list</h1>
+                <div className='container'>
+                    <div className='row'>
+                        {this.props.dog.map(d => (
+                            <div className='col-4 text-center' key={d.name}>
+                                <img src={d.src} alt={d.name}/>
+                                <h3>{d.name}</h3>
+                            </div>
+                        ))}
+                    </div>
+                </div>
              </div>
         );
     }
