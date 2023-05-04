5100ae2bef3518c492866b5245168f6e5be6be92
unknown <sanchit2005yudavanshi@gmail.com>
2023-05-04 21:50:15 +0530

    added copytoclipboard to the colobox component and it is working

--- color-app/src/ColorBox.js
+++ color-app/src/ColorBox.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './ColorBox.css';
-import {CopyToClipBoard} from 'react-copy-to-clipboard';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class ColorBox extends Component{
     render() {
         const {name, background} = this.props;
         return (
+            <CopyToClipboard text={background}>
              <div className='ColorBox' style={{background}}>
                 <div className='copy-container'>
                     <div className='box-content'>
@@ -15,6 +16,7 @@ class ColorBox extends Component{
                 </div>
                 <span className='see-more'>More</span>
              </div>
+            </CopyToClipboard>
         );
     }
 }
