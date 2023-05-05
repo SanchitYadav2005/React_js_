868f523b50aa76f96ca1e73f4a715077475cf89c
unknown <sanchit2005yudavanshi@gmail.com>
2023-05-05 21:18:22 +0530

    chroma js is working and changing the color into hex, rgb and rgba

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -1,12 +1,18 @@
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[4]}/>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    console.log(generatePalette({...seedColors[4]}))
+    return (
+      <div className="App">
+        <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
 }
 
 export default App;
