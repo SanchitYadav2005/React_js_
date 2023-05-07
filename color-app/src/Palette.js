11b456511654d354c8da833b9a7000a6b76521a3
unknown <sanchit2005yudavanshi@gmail.com>
2023-05-07 20:25:50 +0530

    styled the slider Note:- always put the slider css above you own css otherwise it will override your css file unless you have used important tag in your css file

--- color-app/src/Palette.js
+++ color-app/src/Palette.js
@@ -1,43 +1,46 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+// this is the css file of that slider component.
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 // this is a react component that we can use to create slider preaty easily .
 import Slider from 'rc-slider';
-// this is the css file of that slider component.
-import 'rc-slider/assets/index.css';
 
-class Palette extends Component{
-    constructor(props){
+
+class Palette extends Component {
+    constructor(props) {
         super(props)
         // here we are defining a starting level of colors.
-        this.state = {level: 500}
+        this.state = { level: 500 }
         this.changeLevel = this.changeLevel.bind(this)
     }
-    changeLevel(level){
+    changeLevel(level) {
         // changing the state accoding to the value on the slider. You will get an error through map if you did'nt decide how many steps you want to go.
-        this.setState({level})
+        this.setState({ level })
     }
     render() {
         // just refactored this.props.palette.colors and this.state.level.
-        const {colors} = this.props.palette;
-        const {level} = this.state;
+        const { colors } = this.props.palette;
+        const { level } = this.state;
         // maping over every color and displaying using our ColorBox component
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name}/>
+            <ColorBox background={color.hex} name={color.name} />
         ))
         return (
-             <div className='Palette'>
-                <Slider 
-                    defaultValue={level} 
-                    min={100} 
-                    max={900}
-                    step={100}
-                    onChange={this.changeLevel}
-                />
+            <div className='Palette'>
+                <div className='slider'>
+                    <Slider
+                        defaultValue={level}
+                        min={100}
+                        max={900}
+                        step={100}
+                        onChange={this.changeLevel}
+                    />
+                </div>
                 {/* navbar goes here */}
                 <div className='Palette-colors'>{colorBoxes}</div>
                 {/* footer goes here */}
-             </div>
+            </div>
         );
     }
 }
