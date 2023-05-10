896a2de5df48c906596e5aede7730b9a899d79af
unknown <sanchit2005yudavanshi@gmail.com>
2023-05-10 21:10:44 +0530

    added react-router-dom and seted up demo routes

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -2,16 +2,25 @@ import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import { Component } from "react";
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-      // in here we are just getting some sorted of color data that contains the color name, id and other infos about the color
-      <div className="App">
-        {/* here we are parsing that seedColors into a function that we a are importing from colorHelpers file other infos about this funciton will be in that file. We are sending this information about the color ot palette component that helps us to display it. */}
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route
+          exact path="/" render={() => <h1>Palette list goes here</h1>}
+        />
+        <Route
+          exact path="/palette/:id" render={() => <h1>individual palette</h1>}
+        />
+      </Switch>
+      // // in here we are just getting some sorted of color data that contains the color name, id and other infos about the color
+      // <div className="App">
+      //   {/* here we are parsing that seedColors into a function that we a are importing from colorHelpers file other infos about this funciton will be in that file. We are sending this information about the color ot palette component that helps us to display it. */}
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
