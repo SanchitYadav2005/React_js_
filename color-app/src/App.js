84a057301a64c3edeb021b36c21fda6ab6ec4b5a
unknown <sanchit2005yudavanshi@gmail.com>
2023-05-11 21:17:23 +0530

    tried to resolve the reloding issue but not able to do it

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -1,9 +1,9 @@
+import React, { Component } from "react";
+import {Route, Switch} from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
-import { Component } from "react";
 import PaletteList from "./PaletteList";
-import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   findPalette(id){
@@ -15,10 +15,16 @@ class App extends Component {
     return (
       <Switch>
         <Route
-          exact path="/" render={() => <PaletteList palettes={seedColors}/>}
+          exact
+          path="/"
+          render={() => 
+          <PaletteList 
+          palettes={seedColors}
+          />}
         />
         <Route
-          exact path="/palette/:id" 
+          exact
+          path="/palette/:id" 
           render={(routeProps) => 
           <Palette 
           palette={generatePalette(this.findPalette(routeProps.match.params.id))}
