b6a316cea0624d26a55f53cfc11b0312f1955a6d
unknown <sanchit2005yudavanshi@gmail.com>
2023-05-17 20:29:09 +0530

    added a component for single color palette and added that into the route

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -4,6 +4,7 @@ import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import PaletteList from "./PaletteList";
+import SingleColorPalette from "./SingleColorPalette";
 
 class App extends Component {
   findPalette(id){
@@ -37,7 +38,7 @@ class App extends Component {
         <Route
           exact
           path="/palette/:paletteId/:colorId"
-          render={() => <h1>colors</h1>}
+          render={() => <SingleColorPalette/>}
         />
       </Switch>
       
