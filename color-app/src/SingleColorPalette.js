45c2e61e48da90b1ab2d0bd9aa274cef240b2e77
unknown <sanchit2005yudavanshi@gmail.com>
2023-05-18 17:43:54 +0530

    gathering all the shades of the color

--- color-app/src/SingleColorPalette.js
+++ color-app/src/SingleColorPalette.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class SingleColorPalette extends Component{
+    constructor(props){
+        super(props)
+        this._shades = this.gatherShades(this.props.palette, this.props.colorId)
+    }
+    gatherShades(palette, colorToFilterBy){
+        let shades = []
+        let allColors = palette.colors;
+        for(let key in allColors){
+            shades = shades.concat(
+                allColors[key].filter(color => color.id === colorToFilterBy)
+            );
+        }
+        return shades.slice(1);
+    }
     render() {
         return (
              <div>
