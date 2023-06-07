e9d03990d6bcd806684e4cc3178b6b24aa7f87e6
unknown <sanchit2005yudavanshi@gmail.com>
2023-06-07 10:58:45 +0530

    hook is wokring

---
+++ react_hooks/src/Toggler.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import useToggle from './hooks/useToggle';
+
+function Toggler(){
+    const [isHappy, toggleIsHappy] = useToggle(true);
+    return(
+        <div>
+            <h1 onClick={toggleIsHappy}>{isHappy ? "😀" : "😢"}</h1>
+        </div>
+    )
+}
+
+export default Toggler;
