a9fd148651f643f9f723d45190b959fc1e7963ea
unknown <sanchit2005yudavanshi@gmail.com>
2023-07-02 14:57:05 +0530

    removed app.css and added some material ui components that I am going to use

--- todoappwithhooks/src/TodoApp.js
+++ todoappwithhooks/src/TodoApp.js
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
 
 function TodoApp(){
     return(
