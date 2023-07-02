ac783fdf32611b891461f4aec9a38698822019b2
unknown <sanchit2005yudavanshi@gmail.com>
2023-07-02 15:16:35 +0530

    created Todo list with the use of sample data

--- todoappwithhooks/src/TodoApp.js
+++ todoappwithhooks/src/TodoApp.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Grid from '@mui/material/Grid';
-
+import Todolist from './Todolist';
 function TodoApp(){
+    const initialTodos = [
+        {id: 1, task: "Clean fish tank", completed: false},
+        {id: 2, task: "Wash car", completed: true},
+        {id: 3, task: "Grow Beard", completed: false}
+    ]
+    const [todos, setTodos] = useState(initialTodos)
     return(
-        <h1>Todos</h1>
+        <Paper 
+            style={{
+                padding:0,
+                margin: 0,
+                height: "100vh",
+                backgroundColor: "#fafafa"
+            }}
+            elevation={0}
+        >
+            <AppBar color='primary' position='static' style={{height: "64px"}}>
+                <Toolbar>
+                    <Typography color="inherit">Todos with hooks</Typography>
+                </Toolbar>
+            </AppBar>
+            <Todolist todos={todos}/>
+        </Paper>
     )
 }
 
---
+++ todoappwithhooks/src/Todolist.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+
+
+function Todolist(props) {
+    return (
+        <Paper>
+            <List>
+                {props.todos.map(todo => (
+                    <>
+                        <ListItem>
+                            <ListItemText>{todo.task}</ListItemText>
+                        </ListItem>
+                        <Divider />
+                    </>
+                ))}
+            </List>
+        </Paper>
+    )
+}
+
+export default Todolist;
