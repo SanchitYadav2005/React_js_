92a5610cbb1207128ca04e6221fad46cb2de2519
unknown <sanchit2005yudavanshi@gmail.com>
2023-07-03 05:56:34 +0530

    installed material ui icons and seted up them in todo component

--- todoappwithhooks/src/Todo.js
+++ todoappwithhooks/src/Todo.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ListItem from '@mui/material/ListItem';  
 import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 function Todo({task, completed}){
     return(
         <ListItem>
+            <Checkbox tabIndex={-1} checked={completed}/>
             <ListItemText style={{textDecoration: completed ? "line-through" : "none"}}>{task}</ListItemText>
-            
+            <ListItemSecondaryAction>
+                <IconButton>
+                    <DeleteIcon/>
+                </IconButton>
+                <IconButton>
+                    <EditIcon/>
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     )
 }
