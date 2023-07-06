7b2b1749029c6d49d347c3dc70fb951bdfc702b4
unknown <sanchit2005yudavanshi@gmail.com>
2023-07-06 13:38:08 +0530

    imported all the things form material ui we are going to use

---
+++ react_context/src/Form.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+import Avatar from '@mui/material/Avatar';
+import { Button } from "@mui/material";
+import CssBaseline from "@mui/material";
+import FormControl from "@mui/material";
+import FormControlLabel from "@mui/material";
+import Checkbox from "@mui/material";
+import Input from "@mui/material";
+import InputLabel from "@mui/material";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Paper from "@mui/material";
+import Typography from "@mui/material";
+import MenuItem from "@mui/material";
+import Select from "@mui/material";
+
+
+class Form extends Component{
+    render(){
+        return(
+            <h1>Form</h1>
+        )
+    }
+}
+
+export default Form;
