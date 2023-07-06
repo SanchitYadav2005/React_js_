7b7973a4d9cb3c089d4a4d155a899bef560f0263
unknown <sanchit2005yudavanshi@gmail.com>
2023-07-06 12:41:50 +0530

    created navbar

--- react_context/src/Navbar.js
+++ react_context/src/Navbar.js
@@ -1,8 +1,37 @@
 import React, {Component} from 'react';
+import AppBar from '@mui/material/AppBar';
+import ToolBar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import Switch from '@mui/material/Switch';
+
+
 
 class Navbar extends Component{
     render(){
-        return <h1>navbar goes here</h1>
+        return (
+            <div>
+                <AppBar position='static' color='primary'>
+                    <ToolBar>
+                        <IconButton>
+                            <span>spain</span>
+                        </IconButton>
+                        <Typography variant='h6' color="inherit">
+                            App title
+                        </Typography>
+                        <Switch/>
+                        <div className={classes.grow}/>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon/>
+                            </div>
+                        </div>
+                    </ToolBar>
+                </AppBar>
+            </div>
+        )
     }
 }
 
